a32121b3ee0cb471ff0e48e1746639acdce3f5d5
Weston Walker <walk8919@gmail.com>
2023-12-18 12:32:36 -0600

    updated auth pages.

--- Spark.Library/Extensions/SessionExtensions.cs
+++ Spark.Library/Extensions/SessionExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace SparkPoc.Lib.Extensions;
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Spark.Library.Extensions;
 
 public static class SessionExtensions
 {
@@ -12,11 +14,11 @@ public static class SessionExtensions
 		string? message = context.Session.GetString(key);
 		context.Session.Remove(key);
 		return message;
-    }
+	}
 
-    public static bool HasKey(this HttpContext context, string key)
-    {
-        string? message = context.Session.GetString(key);
-        return string.IsNullOrEmpty(message) ? false : true;
-    }
+	public static bool HasKey(this HttpContext context, string key)
+	{
+		string? message = context.Session.GetString(key);
+		return string.IsNullOrEmpty(message) ? false : true;
+	}
 }
